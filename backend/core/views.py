c3779076c649441b6fd2654f7fcc8b820316865d
Mohammad Hossein Yaghoubi Bojmaeh <mhossein.yaghubi20@gmail.com>
2025-12-18 22:23:31 +0330

    UPDATE views

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,13 +1,13 @@
 # from django.shortcuts import render
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # from rest_framework.permissions import IsAuthenticated
 
-# class HealthCheckView(APIView):
-#     permission_classes = [IsAuthenticated]
+class HealthCheckView(APIView):
+    permission_classes = [IsAuthenticated]
 
-#     def get(self, request):
-#         return Response({"status": "ok"})
+    def get(self, request):
+        return Response({"status": "ok"})
 # ------------------------------------------------------------
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
