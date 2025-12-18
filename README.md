369e35f8c0cc68f8862e7753fe2e882be3c4d751
Mohammad Hossein Yaghoubi Bojmaeh <mhossein.yaghubi20@gmail.com>
2025-12-18 19:35:30 +0330

    add core & config & accounts

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# WatchTower
+# WatchTower is a monitoring SaaS backend
+built with Django REST Framework.
+It provides uptime monitoring and alerts.
---
+++ backend/core/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.conf import settings
+
+class service(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASECADE,
+        related_name="services"
+    )
+
+    name = models.CharField(max_length=100)
+    url = models.URLField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.url}"
