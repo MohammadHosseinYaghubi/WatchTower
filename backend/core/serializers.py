2a94fe8053682f901476b0963232f3bd11c083fb
Mohammad Hossein Yaghoubi Bojmaeh <mhossein.yaghubi20@gmail.com>
2025-12-18 19:48:03 +0330

    update serializer

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from . models import Service
+
+class ServiceSeriallizer(serializers.ModelSerializer):
+    class Meta:
+        model = Service
+        fields = ["id", "name", "url", "is_active", "created_at"]
