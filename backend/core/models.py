8fa9da23cf535bc14f9b0aa653d29acf2dbe37c0
Mohammad Hossein Yaghoubi Bojmaeh <mhossein.yaghubi20@gmail.com>
2025-12-18 19:43:45 +0330

    add serializer

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 
-class service(models.Model):
+class Service(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
