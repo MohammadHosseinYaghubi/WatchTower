39611376a25d87e1bb275973c1b3a21fea5ac01a
Mohammad Hossein Yaghoubi Bojmaeh <mhossein.yaghubi20@gmail.com>
2025-12-18 22:20:46 +0330

    add views

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
+# from django.shortcuts import render
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework.permissions import IsAuthenticated
+
+# class HealthCheckView(APIView):
+#     permission_classes = [IsAuthenticated]
+
+#     def get(self, request):
+#         return Response({"status": "ok"})
+# ------------------------------------------------------------
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from .models import Service
+from .serializers import ServiceSeriallizer
 
-class HealthCheckView(APIView):
+class ServiceViewSet(viewsets.ModelViewSet):
+    serializer_class = ServiceSeriallizer
     permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        return Response({"status": "ok"})
+    
+    def get_queryset(self):
+        return Service.objects.filter(user=self.request.user)
+    
+    def perform_create(self,serializer):
+        serializer.save(user=self.request.user)
